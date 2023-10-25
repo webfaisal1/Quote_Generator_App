5b66005909ae3b9109e847069f9b56b555254f2b
Your Name <you@example.com>
2023-10-25 22:04:48 +0600

    final added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+const api_url = "https://api.quotable.io/random";
+const quote = document.getElementById("quote")
+const author = document.getElementById("author");
+
+async function getQuote(url){
+  const response = await fetch(url)
+  const data = await response.json()
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+getQuote(api_url)
+
+function tweet(){
+  window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML, "Tweet Window", "width=600, height=300" );
+}
